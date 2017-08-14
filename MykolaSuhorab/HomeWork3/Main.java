c61fad43d4365cba24f9bd80a856d06e4435baed
Mykola <mykola.suhorab@gmail.com>
2017-08-14 13:50:09 +0300

    Homework 1 and 3 from Java

---
+++ MykolaSuhorab/HomeWork3/Main.java
@@ -0,0 +1,29 @@
+package home;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+		person1.inputInf();
+		person1.outputInf();
+		person1.changeName("Tony");
+		
+//		Person person2 = new Person();
+//		person2.inputInf();
+//		person2.outputInf();
+//		
+//		Person person3 = new Person();
+//		person3.inputInf();
+//		person3.outputInf();
+//		
+//		Person person4 = new Person();
+//		person4.inputInf();
+//		person4.outputInf();
+//		
+//		Person person5 = new Person();
+//		person5.inputInf();
+//		person5.outputInf();
+	}
+
+}
