9210cb77d17f328f53119f0093a1cd4ef8342fa5
hllstrm <trueplayhard@gmail.com>
2017-08-23 13:14:58 +0300

    Added homework6

--- Homework6/src/homework6/task1/Task1.java
+++ Homework6/src/homework6/task1/Task1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package homework6.task1;
 
 import java.util.HashSet;
@@ -30,36 +29,3 @@ public class Task1 {
 	}
 
 }
-=======
-package homework6.task1;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class Task1 {
-
-	public static void main(String[] args) {
-		Set<Integer> s1 = new HashSet<Integer>();
-		Set<Integer> s2 = new HashSet<Integer>();
-		s1.add(10);
-		s1.add(12);
-		s1.add(3);
-		s2.add(4);
-		s2.add(3);
-		s2.add(10);
-		s2.add(15);
-		s2.add(122);
-		System.out.println("s1 = " + s1.toString());
-		System.out.println("s2 = " + s2.toString());
-
-		Set<Integer> res = new HashSet<Integer>(s1);
-		res.retainAll(s2);
-		System.out.println("Interselection of s1 and s2 = " + res);
-		res = s1;
-		res.addAll(s2);
-		System.out.println("Union of s1 and s2 = " + res);
-
-	}
-
-}
->>>>>>> 59626368d5152004bbb3464c28e5bfbc0954faaa
--- Homework6/src/homework6/task3/Main.java
+++ Homework6/src/homework6/task3/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package homework6.task3;
 
 import java.util.ArrayList;
@@ -36,42 +35,3 @@ public class Main {
 	}
 
 }
-=======
-package homework6.task3;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-
-public class Main {
-
-	public static void main(String[] args) {
-		
-		List<Student> students = new ArrayList<>();
-		students.add(new Student("Oleg", 1));
-		students.add(new Student("Igor", 2));
-		students.add(new Student("Andrii", 5));
-		students.add(new Student("Yurii", 3));
-		students.add(new Student("Nazar", 1));
-		System.out.println("===Sorting by name=== ");
-
-		students.sort(new Student.NameComparator());
-
-		for (int i = 0; i < students.size(); i++) {
-			System.out.println(students.get(i));
-		}
-		
-		System.out.println("\n===Sorting by course===");
-
-		students.sort(new Student.CourseComparator());
-
-		for (int i = 0; i < students.size(); i++) {
-			System.out.println(students.get(i));
-		}
-		
-	}
-
-}
->>>>>>> 59626368d5152004bbb3464c28e5bfbc0954faaa
